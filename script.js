e718c3f9677bb2a1042afb34f7fa482f0bca5c45
sumbula <sumbulathpr@gmail.com>
2024-11-16 00:33:38 +0400

    Both html & js file added

---
+++ script.js
@@ -0,0 +1,31 @@
+// let mainh=document.getElementById("main-title")
+// console.log(mainh);
+
+// let ctr=document.getElementsByClassName("container")
+// console.log(ctr);
+
+// let scn=document.getElementsByClassName("section")
+// console.log(scn);
+
+
+// let tg=document.getElementsByTagName("p")
+// console.log(tg);
+
+
+// let bt=document.querySelector(".btn")//id
+// console.log(bt);
+
+// let scn=document.getElementsByClassName("section")
+// console.log(scn[0]);
+
+
+// let bt=document.querySelectorAll(".btn")
+// console.log(bt);
+
+let tg=document.querySelectorAll("p")
+console.log(tg);
+
+
+
+
+
